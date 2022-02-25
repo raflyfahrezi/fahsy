4234c34ea31e53271db572c0ea3d636756e4a0da
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-25 16:47:08 +0700

    fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,18 @@
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
+const storybookConfig = {
+    framework: '@storybook/react',
+    typescript: { reactDocgen: false },
+    core: {
+        builder: 'webpack5',
+    },
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+    ],
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
 }
+
+module.exports = storybookConfig
