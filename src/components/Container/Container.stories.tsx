d60818a41d26844d6ea65cc2ec5fa76d26130bef
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-26 21:17:25 +0700

    adding container stories

---
+++ src/components/Container/Container.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Container from './index'
+
+const Template: ComponentStory<typeof Container> = (args) => (
+    <Container {...args} />
+)
+
+export const ExtraSmall = Template.bind({})
+export const Small = Template.bind({})
+export const Medium = Template.bind({})
+export const Large = Template.bind({})
+
+ExtraSmall.args = {
+    size: 'xs',
+    children: <p>Hello World</p>,
+}
+
+Small.args = {
+    size: 'sm',
+    children: <p>Hello World</p>,
+}
+
+Medium.args = {
+    size: 'md',
+    children: <p>Hello World</p>,
+}
+
+Large.args = {
+    size: 'lg',
+    children: <p>Hello World</p>,
+}
+
+export default {
+    title: 'Components/Container',
+    component: Container,
+} as ComponentMeta<typeof Container>
