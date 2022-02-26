58721eb3412a82e5bbfc36878d0753154edeb50b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-26 21:14:00 +0700

    fix container types

--- src/components/Container/types.ts
+++ src/components/Container/types.ts
@@ -1,7 +1,23 @@
 import { ReactNode, HTMLAttributes } from 'react'
 
-// Type
+// Initialization
 export type ContainerSizeType = 'xs' | 'sm' | 'md' | 'lg'
+
+export type ContainerSizePixelType = '480px' | '768px' | '1024px' | '1200px'
+
+// Model
+export type ContainerSizeModel = Record<
+    ContainerSizeType,
+    ContainerSizePixelType
+>
+
+// Variable
+export const ContainerSize: ContainerSizeModel = {
+    xs: '480px',
+    sm: '768px',
+    md: '1024px',
+    lg: '1200px',
+}
 
 // Styles
 export type ContainerRootStylesProps = {
@@ -9,7 +25,8 @@ export type ContainerRootStylesProps = {
 }
 
 // Interface
-export interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
+export interface ContainerPropsInterface
+    extends HTMLAttributes<HTMLDivElement> {
     children: ReactNode
     size: ContainerSizeType
 }
