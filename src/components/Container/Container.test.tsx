76f0da64bf232ee49af49cfdadab0995f0771ec2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-26 21:13:50 +0700

    create container test

--- src/components/Container/Container.test.tsx
+++ src/components/Container/Container.test.tsx
@@ -4,7 +4,19 @@ import { render } from '@testing-library/react'
 import Container from '.'
 
 describe('Container Testing', () => {
-    test('Container should render', () => {
+    test('Container size xs should render', () => {
+        render(<Container size='xs'>Hello World</Container>)
+    })
+
+    test('Container size sm should render', () => {
+        render(<Container size='sm'>Hello World</Container>)
+    })
+
+    test('Container size md should render', () => {
         render(<Container size='md'>Hello World</Container>)
+    })
+
+    test('Container size lg should render', () => {
+        render(<Container size='lg'>Hello World</Container>)
     })
 })
