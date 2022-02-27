5d4d42461e5e4d172e321302f3655c35c779bdd7
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-27 23:12:44 +0700

    fix container styles

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -1,14 +1,10 @@
 import styled from 'styled-components'
 
-import { colors } from '@/theme'
-
 import { ContainerRootStylesProps } from './types'
 
 export const ContainerRoot = styled['div']<ContainerRootStylesProps>`
     width: 100%;
     max-width: ${(props) => props.size};
-
-    background-color: ${colors.yellow};
 
     margin: 0 auto;
 `
