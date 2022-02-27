a29678d98af96b6bc3b7a2b16aa7d399176578cf
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-27 15:45:51 +0700

    fix button styles

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+import { colors } from '@/theme'
+
+import { ButtonRootStyles } from './types'
+
+export const ButtonRoot = styled['button']<ButtonRootStyles>`
+    width: ${(props) => (props.fullWidth ? '100%' : 'auto')};
+
+    background-color: ${colors.yellow};
+
+    padding: 0 10px;
+
+    border: none;
+    border-radius: 6px;
+
+    outline: none;
+`
