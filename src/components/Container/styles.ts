be20903243729ad903ea2763e6ce19052af2663a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-27 13:00:04 +0700

    implement some colors

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -1,10 +1,14 @@
 import styled from '@emotion/styled'
 
+import { colors } from '@/theme'
+
 import { ContainerRootStylesProps } from './types'
 
 export const ContainerRoot = styled['div']<ContainerRootStylesProps>`
     width: 100%;
     max-width: ${(props) => props.size};
+
+    background-color: ${colors.yellow};
 
     margin: 0 auto;
 `
