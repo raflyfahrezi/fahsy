1ec1c68324d0d376af5767aeedef9c56b294dc0c
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-28 18:52:42 +0700

    create input styles

---
+++ src/components/Form/Input/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+import { font, common } from '@/theme'
+
+import { InputRootStyles } from './types'
+
+export const InputRoot = styled.div<InputRootStyles>`
+    width: 100%;
+    max-width: ${(props) => (props.fullWidth ? 'none' : '250px')};
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`
+
+export const InputLabel = styled.label`
+    font-size: ${font.size[16]};
+
+    margin-bottom: 6px;
+`
+
+export const InputInput = styled.input`
+    width: 100%;
+
+    padding: 12px;
+
+    font-size: ${font.size[16]};
+
+    border-radius: ${common.borderRadius};
+
+    outline: none;
+`
