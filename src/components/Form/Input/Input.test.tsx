56ca78f1b7b6e1088b005c03995451f4593bc9f5
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-02-28 18:53:54 +0700

    create input test

---
+++ src/components/Form/Input/Input.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Input from '.'
+
+describe('Input Testing', () => {
+    test('Input should render', () => {
+        render(
+            <Input type='month' label='Username' fullWidth>
+                Hello World
+            </Input>
+        )
+    })
+})
