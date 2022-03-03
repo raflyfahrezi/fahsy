5f6ac95ebb77c286b37e6384b5be77799ef0f036
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-03 19:30:06 +0700

    initialize value of colors

--- src/theme/colors/index.ts
+++ src/theme/colors/index.ts
@@ -1,7 +1,23 @@
 import { colorsType } from './types'
 
 const colors: colorsType = {
-    yellow: '#FFDE59',
+    primary: {
+        black: '#1A1A1A',
+        yellow: '#FFE247',
+        subtleBlack: '#282828',
+    },
+    secondary: {
+        red: '#F84141',
+        green: '#46D851',
+        subtleRed: '#FFC2C2',
+        darkGreen: '#007531',
+        darkYellow: '#AB9000',
+    },
+    tertiary: {
+        grey: '#B8B8B8',
+        lineStroke: '#F7F7F7',
+        subtleGrey: '#424242',
+    },
 }
 
 export default colors
