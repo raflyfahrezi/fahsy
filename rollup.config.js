a96c70c01383d3fe2b32dc010b1edbeaad09cc9d
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-03 07:30:02 +0700

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -7,13 +7,6 @@ const packageJson = require('./package.json')
 
 export default [
     {
-        external: ['react'],
-        paths: {
-            react: 'react',
-            'react-dom': 'react-dom',
-        },
-    },
-    {
         input: 'src/index.ts',
         output: [
             {
