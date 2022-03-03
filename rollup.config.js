dd038afb55fde66be7c92254d9ff2d179230a157
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-03 10:04:43 +0700

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import dts from 'rollup-plugin-dts'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 const packageJson = require('./package.json')
 
@@ -23,6 +24,7 @@ export default [
         plugins: [
             resolve(),
             commonjs(),
+            peerDepsExternal(),
             typescript({ tsconfig: './tsconfig.json' }),
         ],
     },
