e12c1e0e25f2ce78f6083149326ffc7208d5b6c9
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-04 20:36:44 +0700

    fix input

--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled'
 
-import { font, common } from '@/theme'
+import { font, colors, common } from '@/theme'
 
 import { InputRootStyles } from './types'
 
@@ -14,19 +14,35 @@ export const InputRoot = styled.div<InputRootStyles>`
 `
 
 export const InputLabel = styled.label`
+    font-weight: bold;
     font-size: ${font.size[16]};
+    font-family: ${font.family};
 
-    margin-bottom: 6px;
+    color: ${colors.tertiary.subtleGrey};
+
+    margin-bottom: 10px;
 `
 
 export const InputInput = styled.input`
     width: 100%;
 
-    padding: 12px;
+    padding: 10px 12px;
+
+    color: ${colors.tertiary.subtleGrey};
 
     font-size: ${font.size[16]};
+    font-family: ${font.family};
 
     border-radius: ${common.borderRadius};
+    border: 2px solid ${colors.tertiary.grey};
+
+    background-color: ${colors.primary.subtleBlack};
 
     outline: none;
+
+    transition: all 0.2s;
+
+    &:focus {
+        border: 2px solid ${colors.primary.yellow};
+    }
 `
