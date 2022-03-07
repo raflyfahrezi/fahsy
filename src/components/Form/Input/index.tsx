08fe0571e47b1da5ff97c2dfaaa5af58c9fc94de
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-07 19:01:19 +0700

    fix input

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import FormRoot from '../Root'
 
-import { InputInput } from './styles'
+import { InputRoot } from './styles'
 import { InputPropsInterface } from './types'
 
 const Input = ({ type, label, fullWidth, ...props }: InputPropsInterface) => {
     return (
         <FormRoot label={label} fullWidth={fullWidth}>
-            <InputInput type={type} {...props} />
+            <InputRoot type={type} {...props} />
         </FormRoot>
     )
 }
