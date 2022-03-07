6ec3fd4590a45c28f78be3d62b61644bc6aa44d7
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-07 18:53:13 +0700

    fix select types

--- src/components/Form/Select/types.ts
+++ src/components/Form/Select/types.ts
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-
 import { HTMLAttributes } from 'react'
 
 // Interface
 export interface SelectPropsInterface
     extends HTMLAttributes<HTMLSelectElement> {
-    //
+    label?: string
+    fullWidth?: boolean
 }
