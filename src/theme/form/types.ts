5b09aed50bb9adc9298075e594585b19cc19730c
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-07 15:47:24 +0700

    create theme form type

---
+++ src/theme/form/types.ts
@@ -0,0 +1,17 @@
+export type formType = {
+    outline: string
+
+    transition: string
+
+    backgroundColor: string
+
+    color: string
+    padding: string
+
+    fontSize: string
+    fontFamily: string
+
+    border: string
+    borderFocus: string
+    borderRadius: string
+}
