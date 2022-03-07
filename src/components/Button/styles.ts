82ad3462f59cb1cc465a796326f816bd1d7c92eb
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-07 14:13:25 +0700

    fix button styles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,22 +4,24 @@ import { font, colors } from '@/theme'
 
 import { ButtonRootStyles } from './types'
 
-export const ButtonRoot = styled['button']<ButtonRootStyles>`
+export const ButtonRoot = styled.button<ButtonRootStyles>`
     width: ${(props) => (props.fullWidth ? '100%' : 'auto')};
     min-width: 120px;
 
     background-color: ${colors.primary.yellow};
 
-    padding: 16px 20px;
+    padding: 12px 20px;
 
     border: none;
     border-radius: 6px;
 
     outline: none;
 
-    cursor: pointer;
+    cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
 
     font-weight: bold;
     font-size: ${font.size[16]};
     font-family: ${font.family};
+
+    transition: all 0.3s;
 `
