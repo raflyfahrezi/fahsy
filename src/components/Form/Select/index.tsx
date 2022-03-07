37bf1ea3ead00bee04cb19e335a74301f4229653
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-07 18:53:33 +0700

    fix select

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import FormRoot from '../Root'
+
+import { SelectRoot } from './styles'
 import { SelectPropsInterface } from './types'
 
-const Select = ({ children, ...props }: SelectPropsInterface) => {
-    return <select {...props}>{children}</select>
+const Select = ({
+    label,
+    fullWidth,
+    children,
+    ...props
+}: SelectPropsInterface) => {
+    return (
+        <FormRoot label={label} fullWidth={fullWidth}>
+            <SelectRoot {...props}>{children}</SelectRoot>
+        </FormRoot>
+    )
 }
 
 export default Select
