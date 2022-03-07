14c88d8d2c856a3485ee67acfd8362daa12b4a06
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-07 15:47:35 +0700

    create theme form

---
+++ src/theme/form/index.ts
@@ -0,0 +1,23 @@
+import { font, colors, common } from '@/theme'
+
+import { formType } from './types'
+
+const form: formType = {
+    padding: '10px 12px',
+    color: colors.tertiary.subtleGrey,
+
+    fontSize: font.size[16],
+    fontFamily: font.family,
+
+    borderRadius: common.borderRadius,
+    border: `2px solid ${colors.tertiary.grey}`,
+    borderFocus: `2px solid ${colors.primary.yellow}`,
+
+    backgroundColor: colors.primary.subtleBlack,
+
+    outline: 'none',
+
+    transition: 'all 0.2s',
+}
+
+export default form
