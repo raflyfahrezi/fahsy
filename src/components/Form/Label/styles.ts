73f287a69a10005b37dcbf09c937b1318d315dd2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-07 16:56:48 +0700

    create form label styles

---
+++ src/components/Form/Label/styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled'
+
+import { font, colors } from '@/theme'
+
+export const LabelRoot = styled.label`
+    margin-bottom: 10px;
+
+    color: ${colors.tertiary.subtleGrey};
+
+    font-weight: bold;
+    font-size: ${font.size[16]};
+    font-family: ${font.family};
+`
