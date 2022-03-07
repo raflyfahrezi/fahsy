edd26bde6deba3c019801c088c5deab17e5118a0
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-07 18:06:17 +0700

    create select types

---
+++ src/components/Form/Select/types.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+import { HTMLAttributes } from 'react'
+
+// Interface
+export interface SelectPropsInterface
+    extends HTMLAttributes<HTMLSelectElement> {
+    //
+}
