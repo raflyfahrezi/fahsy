fe5364317deed86b6c6ecb1d5fa636e6802106a2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-13 00:05:28 +0700

    implement forward ref on input

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 import FormRoot from '../Root'
 
 import { InputRoot } from './styles'
 import { InputPropsInterface } from './types'
 
-const Input = ({ type, label, fullWidth, ...props }: InputPropsInterface) => {
-    return (
-        <FormRoot label={label} fullWidth={fullWidth}>
-            <InputRoot type={type} {...props} />
-        </FormRoot>
-    )
-}
+const Input = forwardRef<HTMLDivElement, InputPropsInterface>(
+    ({ type, label, fullWidth, ...props }: InputPropsInterface, ref) => {
+        return (
+            <FormRoot ref={ref} label={label} fullWidth={fullWidth}>
+                <InputRoot type={type} {...props} />
+            </FormRoot>
+        )
+    }
+)
 
 export default Input
