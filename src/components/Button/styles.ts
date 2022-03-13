e39b37c8b6f4856700241fbeddf54dabfc8f7684
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-13 23:35:09 +0700

    update button styles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,18 +1,39 @@
 import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 
 import { font, colors } from '@/theme'
 
 import { ButtonRootStyles } from './types'
 
+export const ButtonVariantPrimary = () => css`
+    color: ${colors.primary.black};
+
+    background-color: ${colors.primary.yellow};
+
+    &:hover {
+        border: 2px solid ${colors.system.yellow[4]};
+
+        background-color: ${colors.system.yellow[4]};
+    }
+`
+
+export const ButtonVariantSecondary = () => css`
+    color: ${colors.primary.yellow};
+
+    background-color: ${colors.primary.black};
+
+    &:hover {
+        background-color: ${colors.system.yellow[1]};
+    }
+`
+
 export const ButtonRoot = styled.button<ButtonRootStyles>`
     width: ${(props) => (props.fullWidth ? '100%' : 'auto')};
     min-width: 120px;
 
-    background-color: ${colors.primary.yellow};
-
     padding: 12px 20px;
 
-    border: none;
+    border: 2px solid ${colors.primary.yellow};
     border-radius: 6px;
 
     outline: none;
@@ -23,17 +44,16 @@ export const ButtonRoot = styled.button<ButtonRootStyles>`
     font-size: ${font.size[16]};
     font-family: ${font.family};
 
-    transition: all 0.1s;
+    transition: all 0.5s;
+
+    ${(props) =>
+        props.variant === 'primary'
+            ? ButtonVariantPrimary
+            : ButtonVariantSecondary}
 
     &:disabled {
+        color: #cea337;
+
         cursor: not-allowed;
-    }
-
-    &:active {
-        transform: scale(0.95);
-    }
-
-    &:hover {
-        background-color: ${colors.system.yellow[4]};
     }
 `
