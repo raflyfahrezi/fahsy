384de196cfc22ad634398b590ca5d1c207c8cdba
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-13 23:34:56 +0700

    update button types

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,12 +1,17 @@
 import { HTMLAttributes } from 'react'
 
+// Type
+export type ButtonVariantType = 'primary' | 'secondary'
+
 // Styles
 export type ButtonRootStyles = {
     fullWidth?: boolean
+    variant?: ButtonVariantType
 }
 
 // Interface
 export interface ButtonPropsInterface
     extends HTMLAttributes<HTMLButtonElement> {
     fullWidth?: boolean
+    variant?: ButtonVariantType
 }
