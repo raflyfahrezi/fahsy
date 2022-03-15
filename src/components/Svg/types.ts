2daf928686f333e7bd8cbc63aea1e025261e7cf1
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-16 00:42:10 +0700

    adding svg types

---
+++ src/components/Svg/types.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+import { HTMLAttributes } from 'react'
+
+// Interface
+export interface SvgPropsInterface extends HTMLAttributes<HTMLObjectElement> {
+    path: string
+}
