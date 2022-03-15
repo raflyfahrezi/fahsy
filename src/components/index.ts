8fbb3c1e9de2fbaff8204324c79c0dc409d56da9
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-16 00:42:22 +0700

    export svg

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,18 @@
+import Svg from './Svg'
+import { SvgPropsInterface } from './Svg/types'
+
 import Button from './Button'
 import { ButtonPropsInterface } from './Button/types'
 
 import Container from './Container'
 import { ContainerPropsInterface } from './Container/types'
 
-export { Button, ButtonPropsInterface, Container, ContainerPropsInterface }
+export {
+    Svg,
+    SvgPropsInterface,
+    Button,
+    ButtonPropsInterface,
+    Container,
+    ContainerPropsInterface,
+}
 export * from './Form'
