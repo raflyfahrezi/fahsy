c01b1e49a6541701b94740a3e3713004185c968a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-03-15 08:46:19 +0700

    create paragraph styles

---
+++ src/typography/Paragraph/styles.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+
+import { font, colors } from '@/theme'
+
+export const ParagraphRoot = styled.p`
+    color: ${colors.tertiary.grey};
+
+    font-size: ${font.size[16]};
+    line-height: ${font.lineHeight[16]};
+
+    font-family: ${font.family};
+`
